98bd19e66207e957c3eef4601471aa1d71c2c1e3
ilze-d2 <92629883+ilze-d2@users.noreply.github.com>
2022-03-16 20:46:31 +0200

    Delete uzd_22.py

--- uzd_22.py
+++
@@ -1,9 +0,0 @@
-picture = [1,2,3,4,5,6,7,8,9,10,11]
-
-for i in picture:
-    if i == 1 or i == 6 or i == 11:
-        print ("  *********  ")
-    if i == 2 or i == 5 or i == 7 or i == 10:
-        print (" *********** ")  
-    if i == 3 or i == 4 or i == 8 or i == 9:
-        print ("****     ****") 
